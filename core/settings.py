ce816b5f9d4cc993ff8a98adc8deda4c56f72ba6
MasoudRostami <masoudr101@gmail.com>
2026-05-10 17:12:00 +0330

    added .env.example for users too choose if use cloud or local storage

--- core/settings.py
+++ core/settings.py
@@ -99,19 +99,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
+
+DATABASE = os.getenv('DATABASE', 'sqlite').lower()
+if DATABASE == 'sqlite':
+    DATABASES = {
         'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+        }
+else:
+    DATABASES = {
+    'default': {
+    'ENGINE': os.getenv('DB_ENGINE'),
+    'NAME': os.getenv('DB_NAME'),
+    'USER': os.getenv('DB_USER'),
+    'PASSWORD': os.getenv('DB_PASSWORD'),
+    'HOST': os.getenv('DB_HOST'),
+    'PORT': os.getenv('DB_PORT')
+}
 
 }
 
@@ -187,25 +193,42 @@ SPECTACULAR_SETTINGS ={
     'VERSION':'1.0.0'
 }
 
-STORAGES = {
+
+STORAGE_MODE = os.getenv("DJANGO_STORAGE", "local")
+
+if STORAGE_MODE == "s3":
+
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        },
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
+    }
+
+    AWS_S3_ENDPOINT_URL = os.getenv('ARVAN_ENDPOINT')
+    AWS_ACCESS_KEY_ID = os.getenv('ARVAN_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('ARVAN_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('ARVAN_STORAGE_BUCKET_NAME')
+    AWS_S3_FILE_OVERWRITE = False
+    # AWS_DEFAULT_ACL = None
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+        'ACL': 'public-read',
+    }
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.ir-thr-at1.arvanstorage.ir' # or your cloud
+
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+else:
+    STORAGES = {
     "default": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
-AWS_S3_ENDPOINT_URL = 'https://s3.ir-thr-at1.arvanstorage.ir'
-AWS_ACCESS_KEY_ID = os.getenv('ARVAN_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('ARVAN_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.getenv('ARVAN_STORAGE_BUCKET_NAME')
-AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-    'ACL': 'public-read',
-}
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.ir-thr-at1.arvanstorage.ir'
-
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = BASE_DIR / "media"
--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from dj_rest_auth.views import PasswordResetConfirmView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -31,3 +34,6 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
 ]
+
+if settings.DEBUG and settings.STORAGE_MODE == "local":
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
