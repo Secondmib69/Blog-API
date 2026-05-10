682369063c5142b9f2a105bf29cd9db7c4f8692b
MasoudRostami <masoudr101@gmail.com>
2026-05-10 17:23:31 +0330

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -100,25 +100,19 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
-DATABASE = os.getenv('DATABASE', 'sqlite').lower()
-if DATABASE == 'sqlite':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-        }
-else:
-    DATABASES = {
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-    'ENGINE': os.getenv('DB_ENGINE'),
+    'ENGINE': 'django.db.backends.postgresql',
     'NAME': os.getenv('DB_NAME'),
     'USER': os.getenv('DB_USER'),
     'PASSWORD': os.getenv('DB_PASSWORD'),
-    'HOST': os.getenv('DB_HOST'),
-    'PORT': os.getenv('DB_PORT')
-}
-
+    'HOST': 'localhost',
+    'PORT': '5432'
+    }
 }
 
 
