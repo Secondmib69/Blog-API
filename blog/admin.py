8b80c3ab9b207e8c7d093c1298bf30831a92b9dc
MasoudRostami <masoudr101@gmail.com>
2026-05-12 15:49:12 +0330

    more customizations on admin fields and permissions

--- blog/admin.py
+++ blog/admin.py
@@ -20,6 +20,60 @@ class UserAdmin(UserAdmin):
     list_display = ['id', 'username', 'email', 'is_staff']
     list_editable = ['is_staff']
     list_display_links = ['username']
+    fieldsets = (
+                (None, {'fields': ('username', 'password')}),
+                ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone', 'job_title', 'bio')}),
+                ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),}),
+                ('Important dates', {'fields': ('last_login', 'date_joined')})
+                )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username',
+                'password1',
+                'password2',
+                'first_name',
+                'last_name',
+                'email',
+                'phone',
+                'job_title',
+                'bio',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            ),
+        }),
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+
+        readonly_fields = ['job_title', 'phone', 'username', 'password', 'first_name', 'last_name', 'email',
+                'date_joined', 'last_login']
+        if not obj:
+            return []
+        if request.user == obj:
+            return []
+        return readonly_fields
+    
+    def has_change_permission(self, request, obj=None):
+        has_perm = super().has_change_permission(request, obj)
+
+        if obj and obj.is_superuser and request.user != obj:
+            return False
+
+        return has_perm
+    
+    def has_delete_permission(self, request, obj=None):
+        has_perm = super().has_delete_permission(request, obj)
+
+        if obj and obj.is_superuser and request.user != obj:
+            return False
+
+        return has_perm
 
 
 @admin.register(Comment)
